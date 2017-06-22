4113f11ebf7492bd74e092f49ce1e21696d8aeb4
chris dostert <chris.dostert@nintex.com>
2017-06-21 18:09:09 -0700

    chore) make fail on non 200 statuscode

--- cmd.sh
+++ cmd.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-curl -F "package=@/package" "https://push.fury.io/${apiToken}/${username}"
+url="https://push.fury.io/${apiToken}/${username}"
+
+statusCode=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -F "package=@/package" "$url")
+
+if test "$statusCode" -ne 200; then
+exit 1
+fi
